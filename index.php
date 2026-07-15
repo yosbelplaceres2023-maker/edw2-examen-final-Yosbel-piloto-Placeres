436565c9cda113bce9cdd17c9cf078d37014a1b1
Yosbel <yosbelplaceres.2023@gmail.com>
2026-07-15 08:43:33 -0400

    Ajout du nom sur la page d'accueil

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$nomEtudiant = "À remplacer par votre nom";
+$nomEtudiant = "Yosbel Piloto Placeres";
 
 $titre = "Examen final EDW2";
 $sousTitre = "Déploiement d’un projet PHP existant sur N0C";
